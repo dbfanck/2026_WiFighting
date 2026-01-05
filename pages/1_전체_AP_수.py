18b820b0c28d9e3497f860cb98eee69dfbf09c60
Sonmin <handbear1208@gmail.com>
2026-01-06 00:23:49 +0900

    Feat: 메인 대시보드 페이지 디자인

--- pages/1_전체_AP_수.py
+++ pages/1_전체_AP_수.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
+def icon(emoji: str):
+    """Shows an emoji as a Notion-style page icon."""
+    st.write(
+        f'<span style="font-size: 78px; line-height: 1">{emoji}</span>',
+        unsafe_allow_html=True,
+    )
+
 st.set_page_config(
     page_title="전체 AP 수",
-    page_icon="📍",
+    page_icon="📡",
 )
 
+icon("📡")
 st.title("전체 AP 수")
